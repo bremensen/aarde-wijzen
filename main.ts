1d09908c929c256e6acdd6587e63324de523f102
bremensen <matthieucollege13@gmail.com>
2024-11-22 22:08:43 +0100

    z'tha!çjg

--- main.ts
+++ main.ts
@@ -1,12 +1,9 @@
-input.onGesture(Gesture.Shake, function () {
-    basic.showArrow(ArrowNames.North)
-    basic.pause(200)
-    basic.showArrow(ArrowNames.East)
-    basic.pause(200)
-    basic.showArrow(ArrowNames.South)
-    basic.pause(200)
-    basic.showArrow(ArrowNames.West)
-})
+let ju = 0
 basic.forever(function () {
-	
+    ju = input.compassHeading()
+    if (ju > 45 || ju < 315) {
+        basic.showString("N")
+    } else {
+        basic.showString("")
+    }
 })
